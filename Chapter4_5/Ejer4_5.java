ea6d3605157b2467b7a3b50985f0c201f43e4a3d
Aaron Ontiveros <aronsontiv@gmail.com>
2023-10-10 14:49:56 -0600

    add exc 4_5

---
+++ Chapter4_5/Ejer4_5.java
@@ -0,0 +1,17 @@
+package Chapter4_5;
+
+
+
+public class Ejer4_5 {
+
+    public static void main(String[]args){
+        int suma =0;
+        int x = 1;
+
+
+        suma=suma+x;
+        System.out.printf("La suma es: %d",suma);
+
+
+    }
+}
