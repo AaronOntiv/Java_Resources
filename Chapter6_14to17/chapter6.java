c0b99d2236011ce6cf4875b7f9260cd3c9375166
Aaron Ontiveros <aronsontiv@gmail.com>
2023-10-25 22:43:00 -0600

    add chapter6 ej 6_14,6_15,6_16,6_17

---
+++ Chapter6_14to17/chapter6.java
@@ -0,0 +1,80 @@
+package Chapter6_14to17;
+
+import java.util.Scanner;
+
+public class chapter6 {
+    private static Scanner entrada=new Scanner(System.in);
+    public static void main(String[]args){
+        app();
+
+    }
+
+    public static void app(){
+        double in;
+        System.out.println("Welcome");
+        int aux=1;
+        while (aux!=0){
+            System.out.println("\nPress\n1: For Floor\n2: For Ceil\n3: Hypotenuse Calculation\n4: Multiples\n5: Divisible by 5\n99: For exit\n");
+            aux=entrada.nextInt();
+            switch (aux){
+                case 1:
+                    System.out.println("Dame numero");
+                    in= entrada.nextDouble();
+                    System.out.printf("El Resultado es %d\n",floor(in));
+                    break;
+                case 2:
+                    System.out.println("Dame numero");
+                    in= entrada.nextDouble();
+                    System.out.printf("El Resultado es %d\n",ceil(in));
+                    break;
+                case 3:
+                    System.out.println("Dame el lado a:\n");
+                    double a= entrada.nextDouble();
+                    System.out.println("Dame el lado b:\n");
+                    double hypo= hypotenuse(a,entrada.nextDouble());
+                    System.out.printf("The hypetenuse is: %f\n",hypo);
+                    break;
+                case 4:
+                    System.out.println("Dame multiple:\n");
+                    int num1=entrada.nextInt();
+                    System.out.printf("Dame numero para ver si es multiplo de %d:\n",num1);
+                    int num2=entrada.nextInt();
+                    isMultiple(num1,num2);
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("Error no option");
+                    break;
+            }
+
+        }
+
+    }
+
+    public static int floor(double in){
+        return (int)in;
+    }
+    public static int ceil(double in){
+        if(in%(int)in>0.4){
+            in+=1;
+        }else{
+            return (int)in;
+        }
+        return (int)in;
+    }
+    public static double hypotenuse(double a, double b){
+        double hypo=Math.sqrt(Math.pow(a,2)+Math.pow(b,2));
+        System.out.printf("The hypotenuse with my method is:%f\n",hypo);
+
+        hypo=Math.hypot(a,b);
+        return hypo;
+    }
+    public static void isMultiple(int a, int b){
+        if(b%a!=0){
+            System.out.println("No es multiplo");
+        }else {
+            System.out.println("es multiplo");
+        }
+    }
+}
---
+++ Chapter6_14to17/readme.txt
@@ -0,0 +1,21 @@
+6.14 (Floor and Ceil) Write two methods myFloor and myCeil that take a positive double num
+variable int myFloor(double num) and int myCeil(double num).
+The myFloor method takes num and returns the largest integer number that is less than or equal to x.
+The myCeil function takes num and finds the smallest number that is greater than or equal to x. Do
+not use any Math class methods. Incorporate this method into an application that sends a double
+value to the functions and tests their ability to calculate the required output.
+6.15 (Hypotenuse Calculations) Define a method hypotenuse that calculates the hypotenuse of
+a right triangle when the lengths of the other two sides are given. The method should take two arguments
+of type double and return the hypotenuse as a double. Incorporate this method into an
+application that reads values for side1 and side2 and performs the calculation with the hypotenuse
+method. Use Math methods pow and sqrt to determine the length of the hypotenuse for each of the
+triangles in Fig. 6.14. [Note: Class Math also provides method hypot to perform this calculation.]
+6.16 (Multiples) Write a method isMultiple that determines, for a pair of integers, whether the
+second integer is a multiple of the first. The method should take two integer arguments and return
+true if the second is a multiple of the first and false otherwise. [Hint: Use the remainder operator.]
+Incorporate this method into an application that inputs a series of pairs of integers (one pair at a
+time) and determines whether the second value in each pair is a multiple of the first.
+6.17 (Divisible by 5) Write a method isDivisible that uses the remainder operator (%) to determine
+whether ten input integers are divisible by 5 or not. The method should take an integer argument
+and return true if the integer is divisible by 5 and false otherwise. Incorporate this method
+into an application that inputs a sequence of integers (one at a time) and determines the result.
